7e55299985622869d56f48df735ff86e39c3e917
muazarif12 <m.arif.25125@khi.iba.edu.pk>
2025-03-27 17:07:08 +0500

    All sections from Navbar to LiveInteraction responisve on tablet aswell as mobile

--- src/components/StatsSection.js
+++ src/components/StatsSection.js
@@ -1,18 +1,18 @@
 import React from "react";
 
 const stats = [
-  { value: "800+", label: "Live Courses & Videos", bgColor: "bg-[#eadff8]", textColor: "text-[#4d3e78]" , borderColor: "border-[#c2aafa]"}, // Updated color
-  { value: "200+", label: "Resources", bgColor: "bg-[#ffe0cc]", textColor: "text-[#b74d1a]" , borderColor: "border-[#ffc8a3]"}, // Updated color
-  { value: "3600+", label: "Hours of classes", bgColor: "bg-[#ffdce8]", textColor: "text-[#a54b7a]" , borderColor: "border-[#d6a2b9]"}, // Updated color
-  { value: "93%", label: "Retention Rate", bgColor: "bg-[#d6edd9]", textColor: "text-[#195642]" , borderColor: "border-[#53b07f]"}, // Updated color
+  { value: "800+", label: "Live Courses & Videos", bgColor: "bg-[#eadff8]", textColor: "text-[#4d3e78]" , borderColor: "border-[#c2aafa]"},
+  { value: "200+", label: "Resources", bgColor: "bg-[#ffe0cc]", textColor: "text-[#b74d1a]" , borderColor: "border-[#ffc8a3]"},
+  { value: "3600+", label: "Hours of classes", bgColor: "bg-[#ffdce8]", textColor: "text-[#a54b7a]" , borderColor: "border-[#d6a2b9]"},
+  { value: "93%", label: "Retention Rate", bgColor: "bg-[#d6edd9]", textColor: "text-[#195642]" , borderColor: "border-[#53b07f]"},
 ];
 
 const StatsSection = () => {
   return (
     <div className="bg-white py-16">
-      <div className="flex justify-center gap-11">
+      <div className="flex flex-wrap justify-center gap-6">
         {stats.map((stat, index) => (
-          <div key={index} className="flex flex-col items-center w-[250px] h-[141.8px]">
+          <div key={index} className="flex flex-col items-center w-[200px] sm:w-[250px]">
             {/* Number inside rounded box with shadow */}
             <div className={`px-8 py-10 rounded-xl text-6xl font-extrabold ${stat.bgColor} ${stat.textColor} ${stat.borderColor} border shadow-md font-['Inter_Tight']`}>
               {stat.value}
